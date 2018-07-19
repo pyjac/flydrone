ef8c5af8d5595298062bba37a1f4d4c1e7bc4c81
pyjac <oyebanji05@gmail.com>
2018-07-19 23:39:25 +0100

    add done struct

---
+++ drone/drone.go
@@ -0,0 +1,44 @@
+package drone
+
+import (
+	"time"
+	"math/rand"
+	"fmt"
+)
+type drone struct {
+	id string
+	x uint32
+	y uint32
+	speed uint32
+	minSpeed uint32
+	maxSpeed uint32
+}
+
+func (d drone) X() uint32 {
+	return d.x
+}
+
+func (d drone) Id() string {
+	return d.id
+}
+
+func (d drone) Y() uint32 {
+	return d.y
+}
+
+func (d drone) GetSpeed() uint32 {
+	return d.speed 
+}
+
+func (d *drone) Move() {
+	rand.Seed(time.Now().Unix())
+	speed := uint32(rand.Intn(int(d.maxSpeed - d.minSpeed))) + d.minSpeed
+	d.accelerate(speed)
+	fmt.Printf("%+v\n", d)
+}
+
+func (d *drone) accelerate(speed uint32) {
+	d.speed = speed
+	d.x += speed
+	d.y += speed
+}
